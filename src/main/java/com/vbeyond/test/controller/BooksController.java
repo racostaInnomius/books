e1463b9369d81d58cc5e50b688df7a0f391ecfb9
Arturo Garcia <arturo.garcia@gtec.com.mx>
2020-05-21 13:24:44 -0500

    Add CraossOrigin

--- src/main/java/com/vbeyond/test/controller/BooksController.java
+++ src/main/java/com/vbeyond/test/controller/BooksController.java
@@ -3,6 +3,7 @@ package com.vbeyond.test.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.vbeyond.test.entities.Book;
 import com.vbeyond.test.exception.ResourceNotFoundException;
 import com.vbeyond.test.repository.BooksRepository;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class BooksController {
